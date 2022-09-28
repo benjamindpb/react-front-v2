d3e5bbafebfffec6976a37a3e317b885b7013ec6
benjamindpb <benjamin.dpb@gmail.com>
2022-09-28 19:39:16 -0400

    v0.1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './App.css'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
