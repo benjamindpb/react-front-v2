5aa8d0546930e57efb726f84e01d1beaeed4536f
benjamindpb <benjamin.dpb@gmail.com>
2022-11-22 13:42:02 -0400

    UI Update / Limit select implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import { Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+
 
 function App() {
   return (
@@ -16,8 +18,8 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
-      </div>
-      
+        <Footer />
+      </div>      
       </>
   );
 }
